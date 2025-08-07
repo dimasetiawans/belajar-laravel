5ab90ac3cc75ed798a0f5207f41b45e8ad8e0b78
dimasetiawans <office.dimasss@gmail.com>
2025-08-07 10:36:13 +0700

    buat halaman home

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Belajar Laravel</title>
+</head>
+<body>
+    <h3>Data Pegawai</h3>
+    <a href="/pegawai/tambah">+ Tambah Pegawai Baru</a>
+</body>
+</html>
